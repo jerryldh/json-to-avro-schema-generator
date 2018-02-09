89a37e2338ac23b9c3750492e7e30ced2dce8ddb
lidonghui <dli101@its.jnj.com>
2018-02-09 17:47:53 +0800

    add case for int,float

--- src/test/java/com/jerrylee/AcroConverterTest.java
+++ src/test/java/com/jerrylee/AcroConverterTest.java
@@ -28,7 +28,7 @@ public class AcroConverterTest {
     public void convertJson() throws IOException {
     	String ns = ".test";
     	String json = "{\"f1\": \"2018-01-09-14-49-48_5\", \"_UPT_\": 1515480595528, \"_UPUN_\": \"SA-ITS-ADFXD_TST\", \"_INST_\": 1515480595528, \"_INSUN_\": \"SA-ITS-ADFXD_TST\", \"_DELETED_\": false, \"__SYSTEM_NAME__\": \"/hcs/kna1\", \"__operation__\": \"CREATE\"}";
-        String schema = "{\"namespace\":\"com.jnj.adf.adf-kafka\",\"name\":\"com.jnj.adf.adf-kafka\",\"type\":\"record\",\"fields\":[{\"name\":\"f1\",\"type\":\"boolean\"},{\"name\":\"_UPT_\",\"type\":\"long\"},{\"name\":\"_UPUN_\",\"type\":\"boolean\"},{\"name\":\"_INST_\",\"type\":\"long\"},{\"name\":\"_INSUN_\",\"type\":\"boolean\"},{\"name\":\"_DELETED_\",\"type\":\"boolean\"},{\"name\":\"__SYSTEM_NAME__\",\"type\":\"boolean\"},{\"name\":\"__operation__\",\"type\":\"boolean\"}]}";
+        String schema = "{\"namespace\":\"adf-kafka\",\"name\":\"adf-kafka\",\"type\":\"record\",\"fields\":[{\"name\":\"f1\",\"type\":\"boolean\"},{\"name\":\"_UPT_\",\"type\":\"long\"},{\"name\":\"_UPUN_\",\"type\":\"boolean\"},{\"name\":\"_INST_\",\"type\":\"long\"},{\"name\":\"_INSUN_\",\"type\":\"boolean\"},{\"name\":\"_DELETED_\",\"type\":\"boolean\"},{\"name\":\"__SYSTEM_NAME__\",\"type\":\"boolean\"},{\"name\":\"__operation__\",\"type\":\"boolean\"}]}";
         String schemaTest = converter.convert(json, ns).replaceAll("\r\n", "").replaceAll(" ", "");
         Assert.assertEquals(schema, schemaTest);
     }
